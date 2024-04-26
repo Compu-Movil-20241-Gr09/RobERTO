89a27272e840fc3a4918913b50b29e8fddb21f30
Sebastian Saldarriaga <ssaldarriaga.cardona@udea.edu.co>
2024-04-26 14:55:30 -0500

    Add create new routine button

--- app/src/main/java/co/edu/udea/compumovil/gr09_20241/roberto/ui/activities/NewRoutineActivity.kt
+++ app/src/main/java/co/edu/udea/compumovil/gr09_20241/roberto/ui/activities/NewRoutineActivity.kt
@@ -4,14 +4,15 @@ import android.content.res.Configuration
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Send
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -121,6 +122,28 @@ fun NewRoutinePortrait(
             onValueChanged = {onEvent(RoutineEvent.SetSessionTime(it))},
             label = stringResource(R.string.session_time)
         )
+
+        // Create Button
+        Row (
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.End
+        ){
+            Button(
+                onClick = {
+                    onEvent(RoutineEvent.SaveRoutine)
+                },
+                enabled = isNewRoutineValid(state)
+            ) {
+                Row {
+                    Text(text = stringResource(R.string.create))
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.Send,
+                        contentDescription = ""
+                    )
+                }
+            }
+        }
     }
 }
 
@@ -197,4 +220,10 @@ fun NewRoutineLandscape(
             label = stringResource(R.string.session_time)
         )
     }
+}
+
+fun isNewRoutineValid(
+    state: RoutineState
+) : Boolean {
+    return state.title.isNotBlank() && state.frecuency.isNotBlank() && state.sessionTime != 0f
 }
