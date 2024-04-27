a3bc35ecfca61a8bfa33a442c8d4d303f50e9d0e
Jacobo Palacio <jacobo.palacio@udea.edu.co>
2024-04-26 21:25:20 -0500

    Adding NewGoalLandscape

--- app/src/main/java/co/edu/udea/compumovil/gr09_20241/roberto/ui/activities/NewGoalActivity.kt
+++ app/src/main/java/co/edu/udea/compumovil/gr09_20241/roberto/ui/activities/NewGoalActivity.kt
@@ -143,8 +143,85 @@ fun NewGoalLandscape(
     onEvent: (GoalEvent) -> Unit,
     onGoalCreatedNav: () -> Unit
 ) {
-    // Landscape layout implementation
-    // Similar to NewGoalPortrait but with a different layout for landscape mode
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Title
+        Text(
+            modifier = Modifier.padding(bottom = 8.dp),
+            text = stringResource(R.string.new_goal)
+        )
+
+        // Title TextField
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            value = state.title,
+            onValueChange = { onEvent(GoalEvent.SetTitle(it))
+            },
+            label = {
+                Text(text = stringResource(R.string.title))
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next
+            ),
+            singleLine = true
+        )
+
+        // Description TextField
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            value = state.description,
+            onValueChange = {
+                onEvent(GoalEvent.SetDescription(it))
+            },
+            label = {
+                Text(text = stringResource(R.string.description))
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Next
+            ),
+            singleLine = false,
+            maxLines = 5
+        )
+
+        // Steps TextField
+        OutlinedTextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 8.dp),
+            value = state.steps,
+            onValueChange = {
+                onEvent(GoalEvent.SetSteps(it))
+            },
+            label = {
+                Text(text = stringResource(R.string.steps))
+            }
+        )
+
+        // Create Button
+        Button(
+            onClick = {
+                onEvent(GoalEvent.SaveGoal)
+                onGoalCreatedNav()
+            },
+            enabled = isNewGoalValid(state)
+        ) {
+            Row {
+                Text(text = stringResource(R.string.create))
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.Send,
+                    contentDescription = ""
+                )
+            }
+        }
+    }
 }
 
 fun isNewGoalValid( state: GoalState) : Boolean {
