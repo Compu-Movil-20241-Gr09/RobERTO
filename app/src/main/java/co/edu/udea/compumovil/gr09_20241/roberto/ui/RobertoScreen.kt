8962e36525c31facfd726e42ba705a77d70d7a96
Sebastian Saldarriaga <ssaldarriaga.cardona@udea.edu.co>
2024-06-07 18:00:14 -0500

    Add bottom sheet to add new

--- app/src/main/java/co/edu/udea/compumovil/gr09_20241/roberto/ui/RobertoScreen.kt
+++ app/src/main/java/co/edu/udea/compumovil/gr09_20241/roberto/ui/RobertoScreen.kt
@@ -1,35 +1,14 @@
 package co.edu.udea.compumovil.gr09_20241.roberto.ui
 
 import androidx.annotation.StringRes
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.automirrored.filled.List
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -45,6 +24,8 @@ import co.edu.udea.compumovil.gr09_20241.roberto.ui.activities.ListItemsScreen
 
 import co.edu.udea.compumovil.gr09_20241.roberto.ui.activities.NewRoutineScreen
 import co.edu.udea.compumovil.gr09_20241.roberto.ui.activities.NewTaskScreen
+import co.edu.udea.compumovil.gr09_20241.roberto.ui.composables.RobertoBottomAppBar
+import co.edu.udea.compumovil.gr09_20241.roberto.ui.composables.RobertoTopAppBar
 import co.edu.udea.compumovil.gr09_20241.roberto.view_models.GoalViewModel
 import co.edu.udea.compumovil.gr09_20241.roberto.view_models.RoutineViewModel
 import co.edu.udea.compumovil.gr09_20241.roberto.view_models.TaskViewModel
@@ -56,85 +37,6 @@ enum class RobertoScreen(@StringRes val title: Int){
     NewRoutine(title = R.string.new_routine),
     NewGoal(title = R.string.new_goal),
     ListItems(title = R.string.elements_lists)
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun RobertoAppBar(
-    currentScreen: RobertoScreen,
-    canNavigateBack: Boolean,
-    navigateUp:() -> Unit,
-    modifier: Modifier = Modifier
-) {
-    TopAppBar(
-        title = { Text(stringResource(currentScreen.title)) },
-        colors = TopAppBarDefaults.mediumTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primaryContainer
-        ),
-        modifier = modifier,
-        navigationIcon = {
-            if(canNavigateBack){
-                IconButton(onClick = navigateUp) {
-                    Icon(
-                        imageVector = Icons.AutoMirrored.Default.ArrowBack,
-                        contentDescription = stringResource(R.string.back_button)
-                    )
-                }
-            }
-        }
-    )
-}
-
-@Composable
-fun RobertoBottomAppBar(
-    navController: NavHostController,
-    modifier: Modifier = Modifier
-) {
-    var isDropdownMenuExpanded by remember {
-        mutableStateOf(false)
-    }
-
-    BottomAppBar(
-        containerColor = MaterialTheme.colorScheme.primaryContainer
-    ) {
-        Row(
-            modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
-        ) {
-            IconButton(onClick = { navController.navigate(RobertoScreen.ListItems.name) }) {
-                Icon(Icons.AutoMirrored.Filled.List, contentDescription = "List")
-            }
-            IconButton(onClick = { isDropdownMenuExpanded = !isDropdownMenuExpanded }) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
-            }
-            DropdownMenu(
-                expanded = isDropdownMenuExpanded,
-                onDismissRequest = { isDropdownMenuExpanded = false }
-            ) {
-                DropdownMenuItem(
-                    text = { Text(text = stringResource(R.string.new_task)) },
-                    onClick = {
-                        isDropdownMenuExpanded = false
-                        navController.navigate(RobertoScreen.NewTask.name)
-                    }
-                )
-                DropdownMenuItem(
-                    text = { Text(text = stringResource(R.string.new_routine)) },
-                    onClick = {
-                        isDropdownMenuExpanded = false
-                        navController.navigate(RobertoScreen.NewRoutine.name)
-                    }
-                )
-                DropdownMenuItem(
-                    text = { Text(text = stringResource(R.string.new_goal)) },
-                    onClick = {
-                        isDropdownMenuExpanded = false
-                        navController.navigate(RobertoScreen.NewGoal.name)
-                    }
-                )
-            }
-        }
-    }
 }
 
 @Composable
@@ -153,7 +55,7 @@ fun RobertoApp(
 
     Scaffold(
         topBar = {
-            RobertoAppBar(
+            RobertoTopAppBar(
                 currentScreen = currentScreen,
                 canNavigateBack = navController.previousBackStackEntry != null,
                 navigateUp = { navController.navigateUp() }
